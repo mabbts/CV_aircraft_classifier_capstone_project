06c58f74be05ddde96687f62db94c92f286e87aa
Matthew Boarts <matthewboarts@Matthews-MacBook-Pro-2.local>
2025-06-05 21:58:52 -0400

    Added test_use_helpers.ipynd to Jupyter Notebooks

--- src/FGVC_dataset.py
+++ src/FGVC_dataset.py
@@ -1,8 +1,9 @@
-# need to import this for the function to work: from datasets import load_dataset
+from datasets import load_dataset
 
 def download_FGVC():
+    
     dataset_train = load_dataset("Multimodal-Fatima/FGVC_Aircraft_train")
     dataset_test = load_dataset("Multimodal-Fatima/FGVC_Aircraft_test")
     embeddings_test = load_dataset("Multimodal-Fatima/FGVC_Aircraft_test_embeddings")
     embeddings_train = load_dataset("Multimodal-Fatima/FGVC_Aircraft_test_embeddings")
-    return dataset_train, dataset_test, embeddings_train, embeddings_test
+    return dataset_train["train"], dataset_test["test"], embeddings_train["openai_clip_vit_large_patch14"], embeddings_test
