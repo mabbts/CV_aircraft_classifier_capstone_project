5931127b0713da146cb00d536dc41c0a46622b73
Chih-Pin Cho <chocp@umich.edu>
2025-08-05 23:17:46 +0800

    All cleaned

--- src/FGVC_dataset.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_dataset
-
-def download_FGVC():
-    
-    dataset_train = load_dataset("Multimodal-Fatima/FGVC_Aircraft_train")
-    dataset_test = load_dataset("Multimodal-Fatima/FGVC_Aircraft_test")
-    embeddings_test = load_dataset("Multimodal-Fatima/FGVC_Aircraft_test_embeddings")
-    embeddings_train = load_dataset("Multimodal-Fatima/FGVC_Aircraft_test_embeddings")
-    return dataset_train["train"], dataset_test["test"], embeddings_train["openai_clip_vit_large_patch14"], embeddings_test
--- src/mac_mps_torch_check.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-if torch.backends.mps.is_available():
-    print("MPS (Apple Metal GPU) is available")
-else:
-    print("Running on CPU")
